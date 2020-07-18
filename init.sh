cc46bde306a2bdab459cde434ac9db818c870681
FuwariSprit <furufuwa1021@gmail.com>
2020-07-18 16:13:29 +0900

    add: init.shにindexを追加する処理を追加

--- init.sh
+++ init.sh
@@ -1,3 +1,10 @@
 #!/bin/bash -eu
 
 echo 'init process'
+
+MYSQL="mysql -u isucon -pisucon"
+$MYSQL <<EOF
+USE isucon;
+ALTER TABLE memos ADD INDEX index_user(user);
+ALTER TABLE memos ADD INDEX index_is_private(created_at);
+EOF
