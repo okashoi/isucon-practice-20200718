a24b85a4711d3b950bf3d4bf60e85944a4e51d8d
isucon <isucon@example.com>
2020-07-18 10:46:24 +0000

    add export

--- init.sh
+++ init.sh
@@ -8,7 +8,7 @@ ALTER TABLE memos ADD INDEX index_user(user);
 ALTER TABLE memos ADD INDEX index_created_at(created_at);
 ALTER TABLE memos ADD summary text NULL;
 EOF
-
+export ISUCON_ENV=prod
 pushd /opt/isucon3-mod/app/tools/init_summary/src
 ./main
 popd
